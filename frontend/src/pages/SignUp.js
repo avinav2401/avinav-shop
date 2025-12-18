585f32323511e6e1f96e65583f5e104606f27695
Avinav priyadarshi <166238765+avinav2401@users.noreply.github.com>
2025-12-18 18:20:07 +0530

    Remove photo upload from signup and add payment button functionality

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -3,147 +3,117 @@ import loginIcons from '../assest/signin.gif'
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
 import { Link, useNavigate } from 'react-router-dom';
-import imageTobase64 from '../helpers/imageTobase64';
 import SummaryApi from '../common';
 import { toast } from 'react-toastify';
 
 const SignUp = () => {
-  const [showPassword,setShowPassword] = useState(false)
-  const [showConfirmPassword,setShowConfirmPassword] = useState(false)
-  const [data,setData] = useState({
-      email : "",
-      password : "",
-      name : "",
-      confirmPassword : "",
-      profilePic : "",
-  })
-  const navigate = useNavigate()
-
-  const handleOnChange = (e) =>{
-      const { name , value } = e.target
-
-      setData((preve)=>{
-          return{
-              ...preve,
-              [name] : value
-          }
-      })
-  }
-
-  const handleUploadPic = async(e) =>{
-    const file = e.target.files[0]
-    
-    const imagePic = await imageTobase64(file)
-    
-    setData((preve)=>{
-      return{
-        ...preve,
-        profilePic : imagePic
-      }
+    const [showPassword, setShowPassword] = useState(false)
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+    const [data, setData] = useState({
+        email: "",
+        password: "",
+        name: "",
+        confirmPassword: "",
     })
+    const navigate = useNavigate()
 
-  }
+    const handleOnChange = (e) => {
+        const { name, value } = e.target
+
+        setData((preve) => {
+            return {
+                ...preve,
+                [name]: value
+            }
+        })
+    }
 
 
-  const handleSubmit = async(e) =>{
-      e.preventDefault()
+    const handleSubmit = async (e) => {
+        e.preventDefault()
 
-      if(data.password === data.confirmPassword){
+        if (data.password === data.confirmPassword) {
 
-        const dataResponse = await fetch(SummaryApi.signUP.url,{
-            method : SummaryApi.signUP.method,
-            headers : {
-                "content-type" : "application/json"
-            },
-            body : JSON.stringify(data)
-          })
-    
-          const dataApi = await dataResponse.json()
+            const dataResponse = await fetch(SummaryApi.signUP.url, {
+                method: SummaryApi.signUP.method,
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
 
-          if(dataApi.success){
-            toast.success(dataApi.message)
-            navigate("/login")
-          }
+            const dataApi = await dataResponse.json()
 
-          if(dataApi.error){
-            toast.error(dataApi.message)
-          }
-    
-      }else{
-        toast.error("Please check password and confirm password")
-      }
+            if (dataApi.success) {
+                toast.success(dataApi.message)
+                navigate("/login")
+            }
 
-  }
+            if (dataApi.error) {
+                toast.error(dataApi.message)
+            }
 
-  return (
-    <section id='signup'>
-        <div className='mx-auto container p-4'>
+        } else {
+            toast.error("Please check password and confirm password")
+        }
 
-            <div className='bg-white p-5 w-full max-w-sm mx-auto'>
+    }
 
-                    <div className='w-20 h-20 mx-auto relative overflow-hidden rounded-full'>
-                        <div>
-                            <img src={data.profilePic || loginIcons} alt='login icons'/>
-                        </div>
-                        <form>
-                          <label>
-                            <div className='text-xs bg-opacity-80 bg-slate-200 pb-4 pt-2 cursor-pointer text-center absolute bottom-0 w-full'>
-                              Upload  Photo
-                            </div>
-                            <input type='file' className='hidden' onChange={handleUploadPic}/>
-                          </label>
-                        </form>
-                    </div>
+    return (
+        <section id='signup'>
+            <div className='mx-auto container p-4'>
+
+                <div className='bg-white p-5 w-full max-w-sm mx-auto'>
 
                     <form className='pt-6 flex flex-col gap-2' onSubmit={handleSubmit}>
-                      <div className='grid'>
-                              <label>Name : </label>
-                              <div className='bg-slate-100 p-2'>
-                                  <input 
-                                      type='text' 
-                                      placeholder='enter your name' 
-                                      name='name'
-                                      value={data.name}
-                                      onChange={handleOnChange}
-                                      required
-                                      className='w-full h-full outline-none bg-transparent'/>
-                              </div>
-                          </div>
+                        <div className='grid'>
+                            <label>Name : </label>
+                            <div className='bg-slate-100 p-2'>
+                                <input
+                                    type='text'
+                                    placeholder='enter your name'
+                                    name='name'
+                                    value={data.name}
+                                    onChange={handleOnChange}
+                                    required
+                                    className='w-full h-full outline-none bg-transparent' />
+                            </div>
+                        </div>
                         <div className='grid'>
                             <label>Email : </label>
                             <div className='bg-slate-100 p-2'>
-                                <input 
-                                    type='email' 
-                                    placeholder='enter email' 
+                                <input
+                                    type='email'
+                                    placeholder='enter email'
                                     name='email'
                                     value={data.email}
                                     onChange={handleOnChange}
                                     required
-                                    className='w-full h-full outline-none bg-transparent'/>
+                                    className='w-full h-full outline-none bg-transparent' />
                             </div>
                         </div>
 
                         <div>
                             <label>Password : </label>
                             <div className='bg-slate-100 p-2 flex'>
-                                <input 
-                                    type={showPassword ? "text" : "password"} 
+                                <input
+                                    type={showPassword ? "text" : "password"}
                                     placeholder='enter password'
                                     value={data.password}
-                                    name='password' 
+                                    name='password'
                                     onChange={handleOnChange}
                                     required
-                                    className='w-full h-full outline-none bg-transparent'/>
-                                <div className='cursor-pointer text-xl' onClick={()=>setShowPassword((preve)=>!preve)}>
+                                    className='w-full h-full outline-none bg-transparent' />
+                                <div className='cursor-pointer text-xl' onClick={() => setShowPassword((preve) => !preve)}>
                                     <span>
                                         {
                                             showPassword ? (
-                                                <FaEyeSlash/>
+                                                <FaEyeSlash />
                                             )
-                                            :
-                                            (
-                                                <FaEye/>
-                                            )
+                                                :
+                                                (
+                                                    <FaEye />
+                                                )
                                         }
                                     </span>
                                 </div>
@@ -153,25 +123,25 @@ const SignUp = () => {
                         <div>
                             <label>Confirm Password : </label>
                             <div className='bg-slate-100 p-2 flex'>
-                                <input 
-                                    type={showConfirmPassword ? "text" : "password"} 
+                                <input
+                                    type={showConfirmPassword ? "text" : "password"}
                                     placeholder='enter confirm password'
                                     value={data.confirmPassword}
-                                    name='confirmPassword' 
+                                    name='confirmPassword'
                                     onChange={handleOnChange}
                                     required
-                                    className='w-full h-full outline-none bg-transparent'/>
+                                    className='w-full h-full outline-none bg-transparent' />
 
-                                <div className='cursor-pointer text-xl' onClick={()=>setShowConfirmPassword((preve)=>!preve)}>
+                                <div className='cursor-pointer text-xl' onClick={() => setShowConfirmPassword((preve) => !preve)}>
                                     <span>
                                         {
                                             showConfirmPassword ? (
-                                                <FaEyeSlash/>
+                                                <FaEyeSlash />
                                             )
-                                            :
-                                            (
-                                                <FaEye/>
-                                            )
+                                                :
+                                                (
+                                                    <FaEye />
+                                                )
                                         }
                                     </span>
                                 </div>
@@ -183,12 +153,12 @@ const SignUp = () => {
                     </form>
 
                     <p className='my-5'>Already have account ? <Link to={"/login"} className=' text-red-600 hover:text-red-700 hover:underline'>Login</Link></p>
+                </div>
+
+
             </div>
-
-
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default SignUp
